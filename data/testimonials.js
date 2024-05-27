d493ba9b53e42ecd6f9213a053ef60d69b10fec7
Edchiii <edchiii@gmail.com>
2024-05-27 20:03:22 +0800

    Testimonial content and footer, change in design and UX

---
+++ data/testimonials.js
@@ -0,0 +1,22 @@
+const testimonials = [
+    {
+        image: 'images/jane.jpg',
+        name: "Jane D.",
+        testimonial: "Iced Spanish Latte is perfection in a cup!."
+    },
+    {
+        image: 'images/mike.jpg',
+        name: "Mike S.",
+        testimonial: "Iced Matcha Espresso is refreshing and energizing!"
+    },
+    {
+        image: 'images/sarah.jpg',
+        name: "Sarah L.",
+        testimonial: "Ice Caramel Macchiato is my sweet indulgence!"
+    },
+    {
+        image: 'images/emma.jpg',
+        name: "Emma R.",
+        testimonial: "Best Iced Spanish Latte I've ever had"
+    },
+]
--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("hello");
+let testimonialHTML ='';
+
+console.log(testimonialHTML);
+
+testimonials.forEach((person) => {
+    testimonialHTML += `
+    <div class="testimonial-container">
+        <p>${person.testimonial}</p>
+        <img class="testimonial-photo" src="${person.image}" alt="">
+        <h3>${person.name}</h3>
+    </div>
+    `;
+
+    document.querySelector('.js-testimonial-display').innerHTML = testimonialHTML;
+
+});
